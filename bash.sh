d5ea8556f44ed35edd08ab99049872cf4fe20538
Wenyuan Lu <wolu0901@hotmail.com>
2022-09-13 15:26:24 -0700

    set up

---
+++ bash.sh
@@ -0,0 +1,13 @@
+# 1. git clone
+git clone https://github.com/MIDS-2022-Fall-Section4-Team1/Neural-Information-Retrieval.git
+# 2. cd secret
+cd secret
+# 3. Change premission
+chmod 400 *.pem
+# 4. Connect to AWS VM
+ssh -i w210.pem ec2-user@ec2-54-219-129-71.us-west-1.compute.amazonaws.com
+
+
+# git commit 
+
+# git push
