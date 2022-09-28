2b7583a3d88adf5211bdf6eb2551bbacdaa2912c
Ubuntu <ubuntu@ip-172-31-17-228.us-west-1.compute.internal>
2022-09-28 03:25:22 +0000

    bash

--- bash.sh
+++ bash.sh
@@ -1,13 +1,38 @@
-# 1. git clone
-git clone https://github.com/MIDS-2022-Fall-Section4-Team1/Neural-Information-Retrieval.git
-# 2. cd secret
-cd secret
-# 3. Change premission
-chmod 400 *.pem
-# 4. Connect to AWS VM
-ssh -i w210.pem ec2-user@ec2-54-219-129-71.us-west-1.compute.amazonaws.com
+# mkdir project 
+# cd project
+# git clone https://github.com/MIDS-2022-Fall-Section4-Team1/Neural-Information-Retrieval
+#chmod 400 *.pem
+
+source activate pytorch
+
+pip install transformers
+pip install omegaconf
+
+cd spalde
+wget https://download.europe.naverlabs.com/splade/sigir22/data.tar.gz
+tar -xzvf data.tar.gz
+
+#jupyter setup
+jupyter notebook password
+
+cd ~
+mkdir ssl
+cd ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
+
+cd ..
+cd project
 
 
-# git commit 
+screen -S jupyter
 
-# git push
+#screen -ls
+#screen -r 1234
+jupyter notebook --certfile=~/ssl/mycert.pem --keyfile ~/ssl/mykey.key
+
+#Ctral a + d
+
+
+#local
+#ssh -i ~/mykeypair.pem -N -f -L 8889:localhost:8888 ubuntu@ec2-54-219-129-71.us-west-1.compute.amazonaws.com
+
