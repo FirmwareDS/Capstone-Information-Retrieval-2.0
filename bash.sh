07990677824b63bfb4e1c73a3323ac92500fa16b
Ubuntu <ubuntu@ip-172-31-25-221.us-west-1.compute.internal>
2022-09-28 04:24:18 +0000

    bash

--- bash.sh
+++ bash.sh
@@ -1,16 +1,29 @@
-# mkdir project 
-# cd project
-# git clone https://github.com/MIDS-2022-Fall-Section4-Team1/Neural-Information-Retrieval
+mkdir project 
+cd project
+git clone https://github.com/MIDS-2022-Fall-Section4-Team1/Neural-Information-Retrieval
 #chmod 400 *.pem
+
+cd Neural-Information-Retrieval/
 
 source activate pytorch
 
+sudo apt-get update
+sudo apt-get install git-lfs
+
 pip install transformers
 pip install omegaconf
+pip install hydra-core
+pip install h5py
+pip install pytrec_eval
+pip install tensorboard
 
-cd spalde
+cd splade
 wget https://download.europe.naverlabs.com/splade/sigir22/data.tar.gz
 tar -xzvf data.tar.gz
+rm data.tar.gz
+cd weights
+git clone https://huggingface.co/naver/splade-cocondenser-ensembledistil
+
 
 #jupyter setup
 jupyter notebook password
@@ -22,7 +35,6 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycer
 
 cd ..
 cd project
-
 
 screen -S jupyter
 
@@ -36,3 +48,13 @@ jupyter notebook --certfile=~/ssl/mycert.pem --keyfile ~/ssl/mykey.key
 #local
 #ssh -i ~/mykeypair.pem -N -f -L 8889:localhost:8888 ubuntu@ec2-54-219-129-71.us-west-1.compute.amazonaws.com
 
+
+#training
+screen -S train
+export SPLADE_CONFIG_NAME="config_splade++_cocondenser_ensembledistil_monogpu.yaml"
+python3 -m splade.all
+#   \ config.checkpoint_dir=monogpu/debug/checkpoint \
+#   config.index_dir=monogpu/debug/index \
+#   config.out_dir=experiments/debug/out
+
+nvidia-smi
